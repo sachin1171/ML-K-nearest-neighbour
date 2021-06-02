c5342936acc50c562be9ebf44e5c89f806c53ec1
sachin <77795400+sachin1171@users.noreply.github.com>
2021-06-02 15:05:51 +0530

    Add files via upload

---
+++ Knn.py
@@ -0,0 +1,123 @@
+####################################Problem 1##########################################
+import pandas as pd
+import numpy as np
+
+glass = pd.read_csv("C:/Users/usach/Desktop/ML-KNN/glass.csv")
+
+# Normalization function 
+def norm_func(i):
+    x = (i-i.min())	/ (i.max()-i.min())
+    return (x)
+
+# Normalized data frame (considering the numerical part of data)
+glass_norm = norm_func(glass.iloc[:, 0:9])
+glass_norm.describe()
+
+X = np.array(glass_norm.iloc[:,:]) # Predictors 
+Y = np.array(glass['Type']) # Target 
+
+#splitting the data ino train and test
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2)
+
+#building the model on train data
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors = 3)
+knn.fit(X_train, Y_train)
+
+#predicting on test data
+pred = knn.predict(X_test)
+pred
+
+# Evaluate the model
+from sklearn.metrics import accuracy_score
+print(accuracy_score(Y_test, pred))
+pd.crosstab(Y_test, pred, rownames = ['Actual'], colnames= ['Predictions']) 
+
+# accuracy on train data
+pred_train = knn.predict(X_train)
+print(accuracy_score(Y_train, pred_train))
+pd.crosstab(Y_train, pred_train, rownames=['Actual'], colnames = ['Predictions']) 
+
+# creating empty list variable 
+acc = []
+
+# running KNN algorithm for 3 to 50 nearest neighbours(odd numbers) and 
+# storing the accuracy values
+
+for i in range(3,50,2):
+    neigh = KNeighborsClassifier(n_neighbors=i)
+    neigh.fit(X_train, Y_train)
+    train_acc = np.mean(neigh.predict(X_train) == Y_train)
+    test_acc = np.mean(neigh.predict(X_test) == Y_test)
+    acc.append([train_acc, test_acc])
+
+import matplotlib.pyplot as plt # library to do visualizations 
+
+# train accuracy plot 
+plt.plot(np.arange(3,50,2),[i[0] for i in acc],"ro-")
+
+# test accuracy plot
+plt.plot(np.arange(3,50,2),[i[1] for i in acc],"bo-")
+
+###########################################Problem 2####################################
+import pandas as pd
+import numpy as np
+
+zoo_data = pd.read_csv("C:/Users/usach/Desktop/ML-KNN/Zoo.csv")
+zoo = zoo_data.iloc[:,1:]
+# Normalization function 
+def norm_func(i):
+    x = (i-i.min())	/ (i.max()-i.min())
+    return (x)
+
+# Normalized data frame (considering the numerical part of data)
+zoo_norm = norm_func(zoo.iloc[:, :16])
+zoo_norm.describe()
+
+X = np.array(zoo_norm.iloc[:,:]) # Predictors 
+Y = np.array(zoo['type']) # Target 
+
+#splitting the data ino train and test
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2)
+
+#building the model on train data
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors = 3)
+knn.fit(X_train, Y_train)
+
+#predicting on test data
+pred = knn.predict(X_test)
+pred
+
+# Evaluate the model
+from sklearn.metrics import accuracy_score
+print(accuracy_score(Y_test, pred))
+pd.crosstab(Y_test, pred, rownames = ['Actual'], colnames= ['Predictions']) 
+
+# accuracy on train data
+pred_train = knn.predict(X_train)
+print(accuracy_score(Y_train, pred_train))
+pd.crosstab(Y_train, pred_train, rownames=['Actual'], colnames = ['Predictions']) 
+
+# creating empty list variable 
+acc = []
+
+# running KNN algorithm for 3 to 50 nearest neighbours(odd numbers) and 
+# storing the accuracy values
+
+for i in range(3,50,2):
+    neigh = KNeighborsClassifier(n_neighbors=i)
+    neigh.fit(X_train, Y_train)
+    train_acc = np.mean(neigh.predict(X_train) == Y_train)
+    test_acc = np.mean(neigh.predict(X_test) == Y_test)
+    acc.append([train_acc, test_acc])
+
+import matplotlib.pyplot as plt # library to do visualizations 
+
+# train accuracy plot 
+plt.plot(np.arange(3,50,2),[i[0] for i in acc],"ro-")
+
+# test accuracy plot
+plt.plot(np.arange(3,50,2),[i[1] for i in acc],"bo-")
